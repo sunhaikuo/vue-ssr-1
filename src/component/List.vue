e0f9a8ca7af00920f25e39b82048dac6909c57fd
sunhaikuo <haikuo.sun@mtime.com>
2018-04-13 16:56:01 +0800

    改善易用性

--- src/component/List.vue
+++ src/component/List.vue
@@ -12,55 +12,55 @@
 </template>
 
 <script>
-    import Vue from 'vue'
-    export default {
-        data() {
-            return {
-                list: []
-            }
-        },
-        methods: {
-            showTitle(title) {
-                alert(title)
-            }
-        },
-        mounted() {
-            Vue.axios.get('http://localhost:5000/data').then((response) => {
-                const list = response.data.data.liveWodList
-                this.list = list
-                console.log(list)
-            })
+import Vue from 'vue'
+export default {
+    data() {
+        return {
+            list: []
         }
+    },
+    methods: {
+        showTitle(title) {
+            alert(title)
+        }
+    },
+    mounted() {
+        Vue.axios.get('http://localhost:7000/data').then(response => {
+            const list = response.data.data.liveWodList
+            this.list = list
+            console.log(list)
+        })
     }
+}
 </script>
 <style lang="less">
-    ul {
-        // width: 1300px;
-        list-style: none;
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        margin: 0 auto;
-        justify-content: center;
-    }
+ul {
+    // width: 1300px;
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    margin: 0 auto;
+    justify-content: center;
+}
 
-    li {
-        width: 380px;
-        height: 270px;
-        margin-right: 30px;
-        background-color: #f2f2f2;
-        cursor: pointer;
-        margin-top: 10px;
-    }
+li {
+    width: 380px;
+    height: 270px;
+    margin-right: 30px;
+    background-color: #f2f2f2;
+    cursor: pointer;
+    margin-top: 10px;
+}
 
-    img {
-        width: 380px;
-        height: 214px;
-    }
+img {
+    width: 380px;
+    height: 214px;
+}
 
-    p {
-        width: 380px;
-        padding-left: 15px;
-        font-size: 16px;
-    }
+p {
+    width: 380px;
+    padding-left: 15px;
+    font-size: 16px;
+}
 </style>
--- tools/dev.js
+++ tools/dev.js
@@ -1,9 +1,11 @@
 const webpack = require('webpack')
 const wpCfg = require('./webpack.js')
 const compiler = webpack(wpCfg.getConfig())
+const server = require('./server')
 compiler.watch({}, (err, stats) => {
     if (err === null && stats.compilation.errors.length === 0) {
         console.log('编译成功')
+        server()
     } else {
         console.log('编译出现错误...')
         console.log(stats.compilation.errors[0].message)
